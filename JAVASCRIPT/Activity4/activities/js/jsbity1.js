209b78595e5a8e69994f7fabadbeb2eaa49bdfd8
hazybless <codetesting2121@gmail.com>
2018-06-25 13:31:44 +0800

    master:modified and adding validations of each file

--- JAVASCRIPT/Activity4/activities/js/jsbity1.js
+++ JAVASCRIPT/Activity4/activities/js/jsbity1.js
@@ -4,13 +4,13 @@ function myname() {
     var person = prompt("Please enter your name", "Blezel Tajor");
 
     if (person === "") {
-    	document.getElementById("demo").innerHTML =
+      document.getElementById("demo").innerHTML =
         "Sorry, you must input a name!! Just hit the refresh icon";
     }else if (person != null) {
         document.getElementById("demo").innerHTML =
         "Hi " + "'" + person + "'" + " ! How are you today?";
     }else 
-   		 document.getElementById("demo").innerHTML =
+       document.getElementById("demo").innerHTML =
         "Sorry, I don't get your name!! Just hit the refresh icon";
 }
 
@@ -19,61 +19,62 @@ var array = [];
 var arrayasc = [];
 var arraydsc = [];
 function myFunction(){
-	
-	var newArray = parseInt(document.getElementById("number").value);
-	if (Number.isNaN(newArray)) return;
-	array.push(newArray);
-	arrayasc.push(newArray);
-	arraydsc.push(newArray);
-	//entered numbers dislay here
-	document.getElementById("demo").innerHTML = array;
-	console.log(array);
-	
-	//entered numbers in ascending order
-	mySort_asc(arrayasc);
-	document.getElementById("asc").innerHTML = array;
-	console.log(mySort_asc(arrayasc));
-	
-	//entered numbers in descending order
-	mySort_dsc(arraydsc);
-	document.getElementById("dsc").innerHTML = array;
-	console.log(mySort_dsc(arraydsc));
+  var newArray = parseInt(document.getElementById("number").value);
+  if (Number.isNaN(newArray)) return;
+  array.push(newArray);
+  arrayasc.push(newArray);
+  arraydsc.push(newArray);
+  //entered numbers dislay here
+  document.getElementById("demo").innerHTML = array;
+  console.log(array);
+  
+  //entered numbers in ascending order
+  mySort_asc(arrayasc);
+  document.getElementById("asc").innerHTML = array;
+  console.log(mySort_asc(arrayasc));
+  
+  //entered numbers in descending order
+  mySort_dsc(arraydsc);
+  document.getElementById("dsc").innerHTML = array;
+  console.log(mySort_dsc(arraydsc));
 }
 //function to sort numbers in ascending order
 function mySort_asc(arr){
-	for(var i = 0; i < array.length; i++) {
-		var temp = array[i];
-		var b = i - 1;
-		while (b >= 0 && array[b] > temp) {
-		  array[b + 1] = array[b];
-		  b--;
-		}
-		array[b + 1] = temp;
-	  }
-	  return array;
+  for(var i = 0; i < array.length; i++) {
+    var temp = array[i];
+    var b = i - 1;
+    while (b >= 0 && array[b] > temp) {
+      array[b + 1] = array[b];
+      b--;
+    }
+    array[b + 1] = temp;
+    }
+    return array;
 }
 
 //function to sort numbers in descending order
 function mySort_dsc(arr){
-	for(var i = 0; i < array.length; i++) {
-		var temp = array[i];
-		var b = i - 1;
-		while (b >= 0 && array[b] < temp) {
-		  array[b + 1] = array[b];
-		  b--;
-		}
-		array[b + 1] = temp ;
-	  }
-	  return array;
+  for(var i = 0; i < array.length; i++) {
+    var temp = array[i];
+    var b = i - 1;
+    while (b >= 0 && array[b] < temp) {
+      array[b + 1] = array[b];
+      b--;
+    }
+    array[b + 1] = temp ;
+    }
+    return array;
 }
 
 //variables for string sort
 var newArray = [];
 function countStr() {
-  var newString = (document.getElementById("number").value);
+  newString = prompt("Enter a text");
+  //var newString = (document.getElementById("number").value);
   if(newString == "") {
+    alert("Please enter a string");
     return;
-  }
+  } else if (newString != null) {
   newArray.push(newString);
 
   document.getElementById("demo").innerHTML = newArray;
@@ -83,6 +84,9 @@ function countStr() {
 
   var desc = countSort_dsc(newArray);
   document.getElementById("demo2").innerHTML = desc;
+  } else
+    alert("You don't enter a string");
+  
 }
 //sort string to ascending
 function countSort_asc(arr) {
